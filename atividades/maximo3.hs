afa9283541b266de05fe3dd25a47a443a0a3ee74
Matheus Henrique <matheushenrique.mh080@gmail.com>
2024-10-04 10:17:41 -0300

    add

--- atividades/maximo3.hs
+++ atividades/maximo3.hs
@@ -1,9 +1,16 @@
+maximo2 :: Int -> Int -> Int
+maximo2 a b = if a > b then a
+             else b
+
 maximo3 :: Int -> Int -> Int -> Int
 maximo3 a b c = if a > b && a > c then a
                 else if b > a && b > c then b
                 else c
-                
+
 maximo3' :: Int -> Int -> Int -> Int
 maximo3' a b c | a > b && a > c = a
                | b >a && b > c = b
                | otherwise = c
+
+maximo3'' :: Int -> Int -> Int -> Int
+maximo3'' a b c = maximo2 c (maximo2 a b)
